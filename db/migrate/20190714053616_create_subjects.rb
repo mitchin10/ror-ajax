c1794dba18333dfde5084dfc6f08312084c76950
gnguyen <gnguyen5464@gmail.com>
2019-07-14 01:38:02 -0400

    Generated Subject scaffold and migrated. Changed root route to subject index page.

---
+++ db/migrate/20190714053616_create_subjects.rb
@@ -0,0 +1,11 @@
+class CreateSubjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subjects do |t|
+      t.string :title
+      t.text :description
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
